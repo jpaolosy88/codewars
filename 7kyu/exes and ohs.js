7adbcb06d9c0c7484500f5c8e6ae247a483802fd
jpaolosy88 <95204492+jpaolosy88@users.noreply.github.com>
2022-09-09 17:57:48 +0800

    daily push for codewars

---
+++ 7kyu/exes and ohs.js
@@ -0,0 +1,14 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+    let arrayStr = str.toLowerCase().split('');
+    return (arrayStr.filter(x => x == 'x').length == arrayStr.filter(x => x == 'o').length);
+  }
