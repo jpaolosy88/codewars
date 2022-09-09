0a7dc7a699142ed0d55bd3c3ab87a0eb81057820
jpaolosy88 <95204492+jpaolosy88@users.noreply.github.com>
2022-09-09 14:36:28 +0800

    daily push for codewars

--- 7kyu/check the exam.js
+++ 7kyu/check the exam.js
@@ -11,3 +11,14 @@
 // checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
 // checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
 
+function checkExam(array1, array2) {
+    let finalScore = 0;
+    for (let i = 0; i < array1.length; i++) {
+      if (array2[i] == array1[i]) {
+        finalScore += 4;
+      } else if (array2[i] != "") {
+        finalScore += -1;
+      }
+    }
+    return (finalScore < 0 ? 0 : finalScore);
+  }
