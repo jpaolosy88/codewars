a6b6835072760cb5caa33d8bf0f8e64ab0786ac9
jpaolosy88 <95204492+jpaolosy88@users.noreply.github.com>
2022-09-14 23:29:09 +0800

    daily push for codewars

---
+++ 6kyu/find the unique number.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/585d7d5adb20cf33cb000235/
+
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+function findUniq(arr) {
+    arr.sort((a,b) => a - b)
+    if (arr.filter(x => x == arr[0]).length == 1) {
+      return arr[0]
+    } else {
+      return arr[arr.length-1]
+    }
+  }
+  
